d464e51394c2973ab275699c134d269348fba656
Adib Aulia Rahmansyah <adibaulia@gmail.com>
2020-01-09 17:06:15 +0700

    change procfile

--- main.go
+++ main.go
@@ -6,21 +6,18 @@ import (
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
+	"github.com/labstack/echo"
 )
 
 func main() {
 
-	// e := echo.New()
-	// e.POST("/", simple)
-	// e.Logger.Fatal(e.Start(":3003"))
-	err := simple()
-	if err != nil {
-
-	}
+	e := echo.New()
+	e.POST("/", simple)
+	e.Logger.Fatal(e.Start(":3003"))
 
 }
 
-func simple() error {
+func simple(c echo.Context) error {
 	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET_KEY"))
 	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
 	httpClient := config.Client(oauth1.NoContext, token)
@@ -29,7 +26,7 @@ func simple() error {
 	client := twitter.NewClient(httpClient)
 
 	// Send a Tweet
-	_, resp, err := client.Statuses.Update("just setting up my twttr", nil)
+	_, resp, err := client.Statuses.Update("just setting up my twttr OKEEYokokokok", nil)
 	if err != nil {
 		log.Print(err)
 	}
