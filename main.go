7ece7d1c5c328af515ca1caef5319c2f4e42ab75
Adib Aulia Rahmansyah <adibaulia@gmail.com>
2020-01-09 16:48:14 +0700

    change some issues

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"log"
 	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
 func main() {
 
+	// e := echo.New()
+	// e.POST("/", simple)
+	// e.Logger.Fatal(e.Start(":3003"))
+	err := simple()
+	if err != nil {
+
+	}
+
+}
+
+func simple() error {
 	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET_KEY"))
 	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
 	httpClient := config.Client(oauth1.NoContext, token)
@@ -21,5 +33,7 @@ func main() {
 	if err != nil {
 		log.Print(err)
 	}
+	log.Print("OKOKOKOKOKO")
 	log.Print(resp)
+	return nil
 }
